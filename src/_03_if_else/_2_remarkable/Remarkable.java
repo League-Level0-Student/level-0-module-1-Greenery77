00dbdb150142a3bca8b968b10a2cd2042f8cac44
root <root@b8c3d9a012d0>
2020-11-09 21:28:05 +0000

    Unbirthday Code :)

--- src/_03_if_else/_2_remarkable/Remarkable.java
+++ src/_03_if_else/_2_remarkable/Remarkable.java
@@ -8,11 +8,33 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
+	
+String Bob = "You make great jokes!";
+String Amie = "Your art is beautiful!";
+String Grace = "You're clever!";
 
 		// 2. Ask the user to enter a name. Store their answer in a variable.
 
+String name = JOptionPane.showInputDialog("Enter a name.");
+
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
-	}
+if (name.equalsIgnoreCase("Bob")) {
+	
+JOptionPane.showMessageDialog(null, Bob); }
+
+
+else if (name.equalsIgnoreCase("Amie")) {
+
+JOptionPane.showMessageDialog(null, Amie);
 }
+
+else if (name.equalsIgnoreCase("Grace")) {
+
+JOptionPane.showMessageDialog(null, Grace); 
+}
+}
+}
+	
+
 
